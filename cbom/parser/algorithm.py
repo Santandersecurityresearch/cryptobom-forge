b4a7eb010064043f2fb1dfe1bc452e17c4ff7d21
Matt Colman <33348891+mtcolman@users.noreply.github.com>
2023-12-12 12:04:53 +0000

    Update algorithm.py

--- cbom/parser/algorithm.py
+++ cbom/parser/algorithm.py
@@ -44,7 +44,7 @@ def parse_algorithm(cbom, codeql_result):
 
 
 def _generate_crypto_component(codeql_result):
-    code_snippet = codeql_result['locations'][0]['physicalLocation']['contextRegion']['snippet']['exactText']
+    code_snippet = utils.extract_precise_snippet(codeql_result)
     algorithm = utils.get_algorithm(code_snippet)
 
     code_snippet = codeql_result['locations'][0]['physicalLocation']['contextRegion']['snippet']['text']
